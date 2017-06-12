3f4eeb0745c1f0fee9b2eb8216a17cc3d5b7afac
Vishwa <vishwagen@gmail.com>
2017-06-12 16:01:19 +0530

    Add files via upload

---
+++ chatwriter.py
@@ -0,0 +1,38 @@
+import json
+import requests
+import time
+_id=1
+url = 'https://verifier:admin123@verifier.cloudant.com/chat/1'
+headers = {
+    'cache-control': "no-cache",
+    'content-type': "application/json",
+    'accept': "application/json"
+    }
+uname=input('Enter your username to continue\n')
+print('Welcome '+uname+' for VISHs CHATROOM')
+username='.........{'+uname+'}'
+
+while True:
+    data = requests.request("GET", url, headers=headers)
+    data_in_text=str(data.text)
+    data_in_json=json.loads(data_in_text.encode())
+    old_id=data_in_json['_id']
+    old_rev=data_in_json['_rev']
+    message=data_in_json['message']
+
+    newmessage=input()
+    payload = {'_id': old_id, '_rev':old_rev, 'message':newmessage}
+    putresponse = requests.request("PUT", url, data=json.dumps(payload))
+    #print(putresponse.text)
+    print(newmessage+username)
+
+
+
+
+
+    #data = requests.request("GET", url, headers=headers)
+    #data_in_text=str(data.text)
+    #data_in_json=json.loads(data_in_text.encode())
+    #new_id=data_in_json['_id']
+    #new_rev=data_in_json['_rev']
+    #message=data_in_json['message']
