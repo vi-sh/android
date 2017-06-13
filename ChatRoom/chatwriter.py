312728b02e984bedd489b924df420a94fd2bcd05
Vishwa <vishwagen@gmail.com>
2017-06-13 11:30:17 +0530

    Add files via upload

---
+++ ChatRoom/chatwriter.py
@@ -0,0 +1,36 @@
+import json
+import requests
+import time
+_id=1
+url = 'https://chatwithyou.cloudant.com/chat/1'
+headers = {
+    'cache-control': "no-cache",
+    'content-type': "application/json",
+    'accept': "application/json"
+    }
+uname=input('Enter your username to continue\n')
+print('Welcome '+uname+' for VISHs CHATROOM')
+print('!!!......Writer Window......!!!')
+username='.........{'+uname+'}'
+
+while True:
+    data = requests.request("GET", url, headers=headers)
+    data_in_text=str(data.text)
+    data_in_json=json.loads(data_in_text.encode())
+    old_id=data_in_json['_id']
+    old_rev=data_in_json['_rev']
+    message=data_in_json['message']
+    spaces='                          '
+    print(spaces+message+username)
+    newmessage=input()
+    time.sleep(1)
+    payload = {'_id': old_id, '_rev':old_rev, 'message':newmessage}
+    putresponse = requests.request("PUT", url, data=json.dumps(payload))
+    #print(putresponse.text)
+    #print(newmessage+username)
+    #data = requests.request("GET", url, headers=headers)
+    #data_in_text=str(data.text)
+    #data_in_json=json.loads(data_in_text.encode())
+    #new_id=data_in_json['_id']
+    #new_rev=data_in_json['_rev']
+    #message=data_in_json['message']
